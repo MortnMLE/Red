e250a05b5048c5bdb9122344b28ce20f99ba4288
Martin Lehmann <martin.lehmann1994.ml@gmail.com>
2026-09-04 18:33:32 +0200

    new function that handles backup creation of documents in case of version conflicts

---
+++ frontend/src/services/documents/backupDocument.js
@@ -0,0 +1,45 @@
+import { authenticatedFetch } from "../accessToken";
+import { POSTnewDocument } from "@/constants/endpoints";
+import { createDocument, createDocumentFlags } from "./documentFactory";
+import { addOrSetLocalRecord } from "../indexedDB/indexedDbApi";
+import { DB_DOCUMENTS } from "@/constants/stores";
+
+export async function backupDocument(document) {
+    const flags = document.flags;
+    flags.dirty = false;
+
+    const backupTitle = document.title + ' - backup';
+
+    const response = await authenticatedFetch(POSTnewDocument, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title: backupTitle,
+            content: document.content,
+            version: document.version,
+            flags
+        })
+    });
+
+    let id = '';
+    if (response.status === 201) {
+        const json = await response.json();
+        id = json.id;
+    }
+
+    const backupDocument = createDocument(
+        id !== '' ? id :document.id + '_backup',
+        document.content,
+        backupTitle,
+        1,
+        createDocumentFlags(
+            false,
+            false,
+            true
+        ),
+    );
+
+    await addOrSetLocalRecord(DB_DOCUMENTS, backupDocument);
+}
