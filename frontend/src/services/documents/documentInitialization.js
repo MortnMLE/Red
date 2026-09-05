bb609014d162db81bbe9ff58c18023b0a761ec96
Martin Lehmann <martin.lehmann1994.ml@gmail.com>
2026-09-05 09:07:10 +0200

    removed console.log

--- frontend/src/services/documents/documentInitialization.js
+++ frontend/src/services/documents/documentInitialization.js
@@ -32,7 +32,6 @@ export async function getDocumentsFromServer() {
 
         return createDocuments(serverDocuments);
     } catch (error) {
-        console.log(`error: ${error}`);
         return [];
     }
 }
